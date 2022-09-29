a950b08e4bd68ea7f9d8b591fdaf9ad7f91ee558
NaragudeNitin <naragudenitin@gmail.com>
2022-09-29 23:57:29 +0530

    Adding code to compare the line

--- LengthOfLine.java
+++ LengthOfLine.java
@@ -6,25 +6,30 @@ import java.lang.Math;
 public class LengthOfLine {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("enter the values of a1,b1,c1");
-		int a1 = sc.nextInt();
-		int b1 = sc.nextInt();
-		int c1 = sc.nextInt();
-		System.out.println("Enter the values of a2, b2, c2");
-		int a2 = sc.nextInt();
-		int b2 = sc.nextInt();
-		int c2 = sc.nextInt();
+		System.out.println("Endpoints of first line");
+		int x1 = 1;
+		int y1 = 4;
+		int x2 = 5;
+		int y2 = 1;
+		System.out.println("Endpoints of Second line");
+		int p1 = 14;
+		int q1 = 19;
+		int p2 = 2;
+		int q2 = 3;
+		double lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		System.out.println("Length of the line with co-ordinates (x1,y1),(" + x1 + "," + y1 + ")" + " and (x2,y2), ("
+				+ x2 + "," + y2 + ") is " + lengthOfLine1);
+		double lengthOfLine2 = Math.sqrt((p2 - p1) * (p2 - p1) + (q2 - q1) * (q2 - q1));
+		System.out.println("Length of the line with co-ordinates (x1,y1),(" + x1 + "," + y1 + ")" + " and (x2,y2), ("
+				+ x2 + "," + y2 + ") is " + lengthOfLine2);
 
-		Integer eq1 = a1 / a2;
-		Integer eq2 = b1 / b2;
-		Integer eq3 = c1 / c2;
-
-		if (eq1.equals(eq2) && eq1.equals(eq3)) {
-			System.out.println("Two lines are identical.");
+		if (lengthOfLine1 == lengthOfLine2) {
+			System.out.println("Two lines are equal");
+		} else if (lengthOfLine1 > lengthOfLine2) {
+			System.out.println("Length of line 1 is greater");
 		} else {
-			System.out.println("Lines are not identical");
+			System.out.println("Length of line 2 is greater");
 		}
-
 		sc.close();
 	}
 }
